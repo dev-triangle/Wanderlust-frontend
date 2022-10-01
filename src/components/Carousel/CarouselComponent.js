dae040699ae5f075ac3fee29a0f1fc9d45e44b77
Adithya Ajith <4adhi007@gmail.com>
2022-10-01 13:39:05 +0530

    carousel finished1

--- src/components/Carousel/CarouselComponent.js
+++ src/components/Carousel/CarouselComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect,useState }  from 'react'
 import baseUrl from '../../utils/Urls'
-import './CarouselComponent.css'
+
 import {Carousel} from 'react-bootstrap'
 
 
