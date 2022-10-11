91d1ddb81741a81ee5cbc275cd9743cf07ed5946
Adithya Ajith <4adhi007@gmail.com>
2022-10-11 20:50:19 +0530

    add

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import './Login.css'
+import '../signup/SignUp.css'
 import Mainlayout from '../../components/Mainlayout/Mainlayout'
 import travel_gif from '../../assets/gifs/world_gif.gif'
 import Aos from 'aos'
@@ -8,7 +8,7 @@ const SignUp = () => {
     useEffect(()=>{
         Aos.init({duration:1100})
     },[])
-    
+
   return (
     <div>
         <Mainlayout>
@@ -16,21 +16,22 @@ const SignUp = () => {
                     <img src={travel_gif} alt="" className='signup_animation' />
                 <div className="signup__form"data-aos="zoom-in"data-aos-delay="150">
                     <div className="su_form_container">
-                        <p className='su__header' >Register</p>
+                        <p className='su__header' >Sign In</p>
                         <div className="data_input_su">
                         <label htmlFor="" className='su__username'>Email: </label>
                             <input className='input__su' placeholder='Email' type="text" />
                        
-                        <label htmlFor=""className='su__username'data-aos="zoom-in"data-aos-delay="150">Username</label>
-                            <input className='input__su' placeholder='Username' type="text" />
+                    
                         
                         <label htmlFor=""className='su__username'data-aos="zoom-in"data-aos-delay="150">Password</label>
                             <input className='input__su' placeholder='Password' type="password" />
 
                             
                         </div>
-                        
+                        <div className='login_buttons'>
+                        <button className="about_btn InBtn"data-aos="zoom-in"data-aos-delay="150">SignIn</button>
                         <button className="about_btn regBtn"data-aos="zoom-in"data-aos-delay="150">Register</button>
+                        </div>
                     </div>
                 </div>
             </div>
