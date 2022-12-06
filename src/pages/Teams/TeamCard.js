616e6f4aa56b913adcc0d5fc15e401b367140597
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-06 20:52:47 +0530

    cleaned teams

--- src/pages/Teams/TeamCard.js
+++ src/pages/Teams/TeamCard.js
@@ -2,19 +2,25 @@ import React from 'react'
 import './Teams.css'
 
 const TeamCard = ({id, name, image}) => {
-    if(id%2===0){
-        return(
-            <div className="team_card team_up">
-            <img src={image} className='team_person_image' alt="" />
-            <p className='team_person_name'> {name} </p>     
-             </div>
-        )
-    }
+//     if(id%2===0){
+//         return(
+//             <div className="team_up">
+//             <div class="card 1">
+//   <div class="card_image"> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+//   <div class="card_title title-white">
+//     <p>Card Title</p>
+//   </div>
+// </div>
+//              </div>
+//         )
+//     }
   return (
-    <div  className="team_card">
-            <img src={image} className='team_person_image' alt="" />
-            <p className='team_person_name'> {name} </p>
+    <div class="card 1">
+    <div class="card_image"> <img src={image} /> </div>
+    <div class="card_title title-white">
+      <p>{name}</p>
     </div>
+  </div>
   )
 }
 
