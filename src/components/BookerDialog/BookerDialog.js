afeaa88a9e1bfd16b8a73ee649277ddc010ba01d
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-09 22:54:05 +0530

    styled dialog container

--- src/components/BookerDialog/BookerDialog.js
+++ src/components/BookerDialog/BookerDialog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Dialog from '@mui/material/Dialog';
 import './BookerDialog.css'
+import dialogvector from '../../assets/svg/dialogvector.svg'
 const BookerDialog = ({handleClose,open,finalSubmit}) => {
   return (
     <div>
@@ -15,14 +16,15 @@ const BookerDialog = ({handleClose,open,finalSubmit}) => {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
+        
         <div className="main_dialog_content">
-          <p>Are You sure you want to book the package ?</p>
+          <p className='confirm__book_que'>Are You sure you want to book the package ?</p>
           <div className="button_continer_dialog">
           <button  className='prev__button' onClick={handleClose}>No</button>
         <button  className='next__button' onClick={finalSubmit}>Yes</button>
           </div>
         </div>
-        
+        <img src={dialogvector} alt="dialog_vecotr" />
       </Dialog>
     </div>
   )
