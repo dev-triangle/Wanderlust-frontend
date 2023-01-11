2d9654516097962c7711e4454d394ec42c151e3b
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-01-11 18:29:02 +0530

    removed animtion content

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Dashboard.css'
 import Mainlayout from '../../components/Mainlayout/Mainlayout'
 import DisplayCards from './DisplayCards'
@@ -6,107 +6,99 @@ import CustomTitle from '../../utils/CustomTitle'
 import axiosInstance from '../../utils/axios'
 import baseUrl from '../../utils/Urls'
 import { useNavigate } from 'react-router-dom'
-import Lottie from 'react-lottie'
-import train_anim from '../../animations/88977-metro-rail.json'
+// import Lottie from 'react-lottie'
+// import train_anim from '../../animations/88977-metro-rail.json'
 import { FaEdit } from 'react-icons/fa'
 import Editprofileform from '../../components/Editprofileform/Editprofileform'
-import {Modal,Button} from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 const Dashboard = () => {
-  const navigate=useNavigate()
-  const[curr_username,setCurr_username]=useState('')
-  const[currEmail,setCurrEmail]=useState('')
-  const[currUserid,setcurrUserid]=useState()
-  const[actualname,setActualname]=useState('')
-  const[phno,setPhno]=useState('')
-  const[userimg,setUserimg]=useState()
-  const[frame,setFrame]=useState('places')
+  const navigate = useNavigate()
+  const [curr_username, setCurr_username] = useState('')
+  const [currEmail, setCurrEmail] = useState('')
+  const [currUserid, setcurrUserid] = useState()
+  const [actualname, setActualname] = useState('')
+  const [phno, setPhno] = useState('')
+  const [userimg, setUserimg] = useState()
+  const [frame, setFrame] = useState('places')
   const [show, setShow] = useState(false);
-  const[userImage,setUserImage]=useState()
+  const [userImage, setUserImage] = useState()
   const handleClose = () => setShow(false);
-  const[userDetailId,setUserDetailId]=useState(0)
-  const parachuteEffect={
-    loop: true,
-        autoplay: true,
-        animationData: train_anim,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-  };
-  useEffect(()=>{
-    axiosInstance.get(`${baseUrl}/current-user/`).then((response)=>{
+  const [userDetailId, setUserDetailId] = useState(0)
+
+  useEffect(() => {
+    axiosInstance.get(`${baseUrl}/current-user/`).then((response) => {
       console.log(response)
       setCurr_username(response.data.username)
       setCurrEmail(response.data.email)
       setcurrUserid(response.data.id)
-      localStorage.setItem('userid',response.data.id)
-    },(error)=>{
+      localStorage.setItem('userid', response.data.id)
+    }, (error) => {
       console.log(error)
     })
-  },[])
-  useEffect(()=>{
-    axiosInstance.get(`${baseUrl}/user-detail/`).then((response)=>{
+  }, [])
+  useEffect(() => {
+    axiosInstance.get(`${baseUrl}/user-detail/`).then((response) => {
       console.log(response)
-      response.data.forEach((item)=>{
-        if(item.user_foreign===currUserid)
-         { 
+      response.data.forEach((item) => {
+        if (item.user_foreign === currUserid) {
           setUserImage(item.user_image)
-         setUserDetailId(item.id)
+          setUserDetailId(item.id)
           setActualname(item.actual_name)
           setPhno(item.phno)
-      }
+        }
       })
     })
   })
   return (
     <div>
-      <CustomTitle title="Dashboard"/>
+      <CustomTitle title="Dashboard" />
       <Mainlayout>
         <div className="dash__main_container">
-          
+
           {/* profile main starts here */}
-        <div className="profile__main">
-        
-        <img className='profile__pic' src={userImage} alt="your_image" />
-        <div className="profile__details">
-          <div className="each_detail_profile"><div className='profile_name'>Name: </div><span className='profile_name_content'>{actualname}</span> </div>
-          <div className="each_detail_profile"><div className='profile_name'>Phone no: </div><p className='profile_name_content'>{phno}</p></div>
-          <div className="each_detail_profile"><div className='profile_name'>Username: </div><p className='profile_name_content'>{curr_username}</p></div>
-          <div className="each_detail_profile"><div className='profile_name'>Email: </div><p className='profile_name_content'>{currEmail}</p></div>
-          <FaEdit size={30} onClick={()=>{ setShow(true) }}/>
-        </div>
-        <Lottie options={parachuteEffect} style={{width:'20%' ,height:'30%'}}/>
-        </div>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-            <Modal.Title>
-                Edit profile
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Editprofileform userDetailId={userDetailId} actualname={actualname} phno={phno} userimg={userimg} setActualname={setActualname} setPhno={setPhno} setUserimg={setUserimg}/>
-        </Modal.Body>
-        <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close Button
-                </Button>
-        </Modal.Footer>
-    </Modal>
-        {/* profile main ends here */}
-        <div className="booking__main">
-          <div className="booked__header_container">
-            <div className="booked__title">My Bookings</div>
-            <div className="booked_tabs">
-              {frame==='places'?<span style={{textDecoration:'underline'}} className="booked_places">Places</span>:<span onClick={()=>{setFrame('places')}} className="booked_places">Places</span>}
-              {frame==='stays'?<span style={{textDecoration:'underline'}} className="booked_places">Stays</span>:<span onClick={()=>{setFrame('stays')}} className="booked_places">Stays</span>}
-              {frame==='travels'?<span style={{textDecoration:'underline'}} className="booked_places">Travels</span>:<span onClick={()=>{setFrame('travels')}} className="booked_places">Travels</span>}
+          <div className="profile__main">
+
+            <img className='profile__pic' src={userImage} alt="your_image" />
+            <div className="profile__details">
+              <div className="each_detail_profile"><div className='profile_name'>Name: </div><span className='profile_name_content'>{actualname}</span> </div>
+              <div className="each_detail_profile"><div className='profile_name'>Phone no: </div><p className='profile_name_content'>{phno}</p></div>
+              <div className="each_detail_profile"><div className='profile_name'>Username: </div><p className='profile_name_content'>{curr_username}</p></div>
+              <div className="each_detail_profile"><div className='profile_name'>Email: </div><p className='profile_name_content'>{currEmail}</p></div>
+              <FaEdit size={30} onClick={() => { setShow(true) }} />
             </div>
+            {/* <Lottie options={parachuteEffect} style={{width:'20%' ,height:'30%'}}/> */}
           </div>
-          <div className='booked_container'>
-          <DisplayCards frame={frame} currUserid={currUserid}/>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                Edit profile
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Editprofileform userDetailId={userDetailId} actualname={actualname} phno={phno} userimg={userimg} setActualname={setActualname} setPhno={setPhno} setUserimg={setUserimg} />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close Button
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          {/* profile main ends here */}
+          <div className="booking__main">
+            <div className="booked__header_container">
+              <div className="booked__title">My Bookings</div>
+              <div className="booked_tabs">
+                {frame === 'places' ? <span style={{ textDecoration: 'underline' }} className="booked_places">Places</span> : <span onClick={() => { setFrame('places') }} className="booked_places">Places</span>}
+                {frame === 'stays' ? <span style={{ textDecoration: 'underline' }} className="booked_places">Stays</span> : <span onClick={() => { setFrame('stays') }} className="booked_places">Stays</span>}
+                {frame === 'travels' ? <span style={{ textDecoration: 'underline' }} className="booked_places">Travels</span> : <span onClick={() => { setFrame('travels') }} className="booked_places">Travels</span>}
+              </div>
+            </div>
+            <div className='booked_container'>
+              <DisplayCards frame={frame} currUserid={currUserid} />
+            </div>
+
+            <button className='newtravel' onClick={() => { navigate('/') }} variant="text">+ New Travel</button>
           </div>
-        
-          <button className='newtravel' onClick={()=>{navigate('/')}} variant="text">+ New Travel</button> 
-        </div>
         </div>
       </Mainlayout>
     </div>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,24 +4,17 @@ import Mainlayout from '../../components/Mainlayout/Mainlayout'
 import CarouselComponent from '../../components/Carousel/CarouselComponent'
 import Places from '../../components/Places/Places'
 import CustomTitle from '../../utils/CustomTitle'
-import Lottie from 'react-lottie'
-import earth_anim from '../../animations/47539-rotating-earth.json'
+// import Lottie from 'react-lottie'
+// import earth_anim from '../../animations/47539-rotating-earth.json'
 
 const LandingPage = () => {
-  const onEarthRotate = {
-    loop: true,
-    autoplay: true,
-    animationData: earth_anim,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+  
   return (
     <div>
       <CustomTitle title="Welcome"/>
       <Mainlayout>
         <CarouselComponent/>
-        <Lottie options={onEarthRotate} style={{width:'20%' ,height:'30%'}}/>
+        {/* <Lottie options={onEarthRotate} style={{width:'20%' ,height:'30%'}}/> */}
         <Places/>
       </Mainlayout>
     </div>
